752b1f521db917f13cdc98a1c7b41c3ea24dd02f
farhanreizha <reizha226@gmail.com>
2022-08-19 11:32:17 +0700

    deleted subtitle on homepage

--- src/components/Home/mainHome.js
+++ src/components/Home/mainHome.js
@@ -6,10 +6,6 @@ const Home = () => {
     <>
       <Container className="my-4">
         <h1>Home Page</h1>
-        <h5>
-          mohon maap aku ngga bisa membuat UI yang bagus tapi untuk logic yang
-          aku buat tidak bagus juga sepertinya wkwk
-        </h5>
         <Profile />
       </Container>
     </>
